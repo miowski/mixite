e62473190da362220fb0c9ae691984be0e2a063a
Phil <philemon.varret@gmail.com>
2021-03-15 18:02:31 +0100

    Instructions BDD : Amélioration gestion types de profil

--- init.sql
+++ init.sql
@@ -3,8 +3,14 @@
 CREATE TABLE Profil
 (
     idProfil           bigint PRIMARY KEY,
-    typeProfil         tinyint,
     dateCreationProfil datetime
+);
+
+CREATE TABLE Groupe
+(
+    idGroupe           bigint PRIMARY KEY,
+    dateCreationGroupe datetime,
+    membresGroupe      varchar(512)
 );
 
 -- Infos de profil (partagées par musiciens ET groupes)
@@ -15,7 +21,14 @@ CREATE TABLE InfoProfil
     descriptionProfil varchar(140),
     photoProfil       bigint,
     idProfil          bigint,
-    FOREIGN KEY (idProfil) REFERENCES Profil (IdProfil)
+    idGroupe          bigint,
+    FOREIGN KEY (idProfil) REFERENCES Profil (IdProfil),
+    FOREIGN KEY (idGroupe) REFERENCES Groupe (idGroupe),
+    -- Vérifie qu'un seul type de profil est sollicité (musicien ou groupe)
+    CONSTRAINT SingleProfileType check (
+            (idProfil IS NULL OR idGroupe IS NULL)
+            AND NOT (idProfil IS NULL AND idGroupe IS NULL)
+        )
 );
 
 CREATE TABLE InfoProfil_Reseaux
@@ -27,7 +40,14 @@ CREATE TABLE InfoProfil_Reseaux
     spotifyUserName   varchar(127),
     deezerUrl         varchar(127),
     idProfil          bigint,
-    FOREIGN KEY (idProfil) REFERENCES Profil (idProfil)
+    idGroupe          bigint,
+    FOREIGN KEY (idProfil) REFERENCES Profil (IdProfil),
+    FOREIGN KEY (idGroupe) REFERENCES Groupe (idGroupe),
+    -- Vérifie qu'un seul type de profil est sollicité (musicien ou groupe)
+    CONSTRAINT SingleProfileType check (
+            (idProfil IS NULL OR idGroupe IS NULL)
+            AND NOT (idProfil IS NULL AND idGroupe IS NULL)
+        )
 );
 
 -- InfoProfil : instruments maitrisés, réservés aux profils de musiciens uniquement
@@ -55,8 +75,13 @@ CREATE TABLE Post
     idPost   bigint PRIMARY KEY,
     datePost datetime,
     typePost tinyint,
-    idProfil bigint,
-    FOREIGN KEY (idProfil) REFERENCES Profil (idProfil)
+    idProfil          bigint,
+    idGroupe          bigint,
+    FOREIGN KEY (idProfil) REFERENCES Profil (IdProfil),
+    FOREIGN KEY (idGroupe) REFERENCES Groupe (idGroupe)
+    -- Inutile de ne sélectionner qu'un seul type de publicateur ici :
+    -- Un utilisateur peut publier, mais aussi un utilisateur au travers d'un groupe,
+    -- auquel cas l'id de l'utilisateur ET du groupe sont sollicités.
 );
 
 -- Médias & texte inclus dans un post
@@ -74,14 +99,24 @@ CREATE TABLE Post_Contenu
 
 -- Réalisations d'un groupe ou d'un musicien
 -- -- releaseType : 0 = single, 1 = Album
+-- -- (Un album est un ensemble de musiques sorties en même temps,
+-- -- tandis qu'un Single est une musique seule)
 
 CREATE TABLE Releases
 (
     releaseId    bigint PRIMARY KEY,
     releaseDate  datetime,
     releaseName  varchar(127),
+    releaseUrl   varchar(127),
     releaseCover bigint,
     releaseType  tinyint,
-    idProfil     bigint,
-    FOREIGN KEY (idProfil) REFERENCES Profil (idProfil)
+    idProfil          bigint,
+    idGroupe          bigint,
+    FOREIGN KEY (idProfil) REFERENCES Profil (IdProfil),
+    FOREIGN KEY (idGroupe) REFERENCES Groupe (idGroupe),
+    -- Vérifie qu'un seul type de profil est sollicité (musicien ou groupe)
+    CONSTRAINT SingleProfileType check (
+            (idProfil IS NULL OR idGroupe IS NULL)
+            AND NOT (idProfil IS NULL AND idGroupe IS NULL)
+        )
 );
