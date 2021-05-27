024700f0250c09031687953622ab1374d85dfd29
Phil <philemon.varret@gmail.com>
2021-05-27 23:04:14 +0200

    Fix missing variables $profile and $instrument
    + Add getters and setters

--- mixite-api/classes/class.PlayedInstrument.php
+++ mixite-api/classes/class.PlayedInstrument.php
@@ -4,6 +4,32 @@ class PlayedInstrument implements JsonSerializable {
 
     private $since = null;
 
+    private int $profile = 0;
+    private int $instrument = 0;
+
+    public function getProfile(): int
+    {
+        return $this->profile;
+    }
+
+    public function setProfile(int $profile): PlayedInstrument
+    {
+        $this->profile = $profile;
+        return $this;
+    }
+
+    public function getInstrument(): int
+    {
+        return $this->instrument;
+    }
+
+    public function setInstrument(int $instrument): PlayedInstrument
+    {
+        $this->instrument = $instrument;
+        return $this;
+    }
+
+
     public function getSince()
     {
         return $this->since;
