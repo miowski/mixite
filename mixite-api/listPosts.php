dc157ed499b88ab043072c542668a856c28efd6a
Phil <philemon.varret@gmail.com>
2021-05-28 22:57:40 +0200

    (WIP) rework listPosts.php

--- mixite-api/listPosts.php
+++ mixite-api/listPosts.php
@@ -1,22 +1,23 @@
 <?php
 
+require_once('cnx.php');
 require_once('classes/class.Post.php');
 
 $pdo = new PDO(
     "mysql:host=localhost;dbname=mixite;charset=utf8",
-"root",
-"",
-"http://localhost//mixite/mixite-api//"
+    "mixite_admin",
+    "password",
+
 );
 
-$sql = "SELECT * FROM Posts WHERE Publisher IS 1 ORDER BY PublicationDate";
+$sql = "SELECT * FROM Posts ORDER BY PublicationDate";
 
 $request = $pdo->prepare($sql);
 $list = array();
 
 if ($request->execute()) {
 
-    while($data = $request.fetch()) {
+    while ($data = $request->fetch()) {
 
         $post = new Post(
             $data['ID'],
@@ -27,6 +28,12 @@ if ($request->execute()) {
             $data['Media'],
         );
 
+        $list[] = $post;
     }
 
 }
+
+echo json_encode($list);
+exit();
+
+?>
