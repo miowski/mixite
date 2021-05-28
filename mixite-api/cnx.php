6449e024cc4d4ec4acda619ed0ea6bab9e697766
Phil <philemon.varret@gmail.com>
2021-05-28 22:59:47 +0200

    Add cnx (?)

---
+++ mixite-api/cnx.php
@@ -0,0 +1,14 @@
+<?php
+
+$parameters = parse_ini_file("param/param.ini");
+
+$pdo = new PDO(
+    $parameters['dsn'],
+    $parameters['user'],
+    $parameters['psw'],
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+);
+
+$host = $parameters['host'];
+
+?>
