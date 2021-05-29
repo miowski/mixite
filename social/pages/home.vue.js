9456b5b16db95c6527db550771fe0eabc3eb10d5
Phil V <philemon.varret@gmail.com>
2021-05-29 10:18:16 +0200

    Create vue for feed
    Add basic CSS

--- social/pages/home.vue.js
+++ social/pages/home.vue.js
@@ -1,24 +1,31 @@
 let Accueil = Vue.component('Accueil', {
-    template:`
+    template: `
+    <main>
     <div class="feed">
         <div class="post">
             <h3 class="title">Premier post !</h3>
             <p class="description">Ceci est le premier post de Mixite.</p>
-            <img class="media">
         </div>
     </div>
+    <div class="feed">
+        <div class="post">
+            <h3 class="title">Premier post !</h3>
+            <p class="description">Ceci est le premier post de Mixite.</p>
+        </div>
+    </div>
+    </main>
 `,
-data() {
+    data() {
         return {
             postList: []
         }
-},
-mounted() {
+    },
+    mounted() {
         axios.get('http://localhost/mixite/mixite-api/listPosts.php')
             .then(response => {
                 this.postList = response.data;
                 console.log("Post list : ", this.postList)
             })
-},
-methods: {}
+    },
+    methods: {}
 })
