30cd6bb4c54d6afec0725f8d6d730f92ad8a3e12
Phil V <philemon.varret@gmail.com>
2021-05-29 10:19:22 +0200

    Add debugger
    Update test credentials

--- mixite-api/cnx.php
+++ mixite-api/cnx.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display_errors', 'On');
+
 $parameters = parse_ini_file("param/param.ini");
 
 $pdo = new PDO(
@@ -10,5 +12,3 @@ $pdo = new PDO(
 );
 
 $host = $parameters['host'];
-
-?>
